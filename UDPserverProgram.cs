bc9bfa7e39c578b05877a1ff43815f6e606ce2a9
Mingkai Ma <mamingkai824@gmail.com>
2016-04-20 23:23:57 +0200

    Update UDPserverProgram.cs

--- UDPserverProgram.cs
+++ UDPserverProgram.cs
@@ -13,30 +13,30 @@ namespace UDP
             int recv;
             byte[] data = new byte[1024];
 
-            //得到本机IP，设置TCP端口号         
+            //Get IP,setup TCP port        
             IPEndPoint ip = new IPEndPoint(IPAddress.Any, 10159);
             Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            //绑定网络地址
+            //Bind network address
             newsock.Bind(ip);
-            Console.WriteLine("我是UDP服务器, 我的名字是{0}", Dns.GetHostName());
-            //等待客户机连接
-            Console.WriteLine("正在等待客户机连接...");
-            //得到客户机IP
+            Console.WriteLine("I am UDP server,my name is{0}", Dns.GetHostName());
+            //Wait for client connection
+            Console.WriteLine("Waiting for client connection...");
+            //get client IP
             IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
             EndPoint Remote = (EndPoint)(sender);
             recv = newsock.ReceiveFrom(data, ref Remote);
-            Console.WriteLine("收到来自{0}的信息: ", Remote.ToString());
+            Console.WriteLine("receive message from{0}: ", Remote.ToString());
             Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
-            //客户机连接成功后，发送信息
+            //Client connection success,send message
             string welcome = "welcome,client! ";
-            //字符串与字节数组相互转换
+            
             data = Encoding.Default.GetBytes(welcome);
-            //发送信息
+            //Send message
             newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
             while (true)
             {
                 data = new byte[1024];
-                //发送接收信息
+                //Send received message
                 recv = newsock.ReceiveFrom(data, ref Remote);
                 Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
                 newsock.SendTo(data, recv, SocketFlags.None, Remote);
