92986ddb2ff80649348b6779572a11b8a4f64050
Mingkai Ma <mamingkai824@gmail.com>
2016-04-20 21:46:21 +0200

    Added files via upload

---
+++ UDPserverProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] data = new byte[1024];
+
+            //得到本机IP，设置TCP端口号         
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 10159);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            //绑定网络地址
+            newsock.Bind(ip);
+            Console.WriteLine("我是UDP服务器, 我的名字是{0}", Dns.GetHostName());
+            //等待客户机连接
+            Console.WriteLine("正在等待客户机连接...");
+            //得到客户机IP
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            Console.WriteLine("收到来自{0}的信息: ", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            //客户机连接成功后，发送信息
+            string welcome = "welcome,client! ";
+            //字符串与字节数组相互转换
+            data = Encoding.Default.GetBytes(welcome);
+            //发送信息
+            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+            while (true)
+            {
+                data = new byte[1024];
+                //发送接收信息
+                recv = newsock.ReceiveFrom(data, ref Remote);
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+                newsock.SendTo(data, recv, SocketFlags.None, Remote);
+            }
+        }
+
+    }
+}
