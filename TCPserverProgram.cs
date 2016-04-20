46f109fbfd95bc627e512dde796fc5a28cc643f8
Mingkai Ma <mamingkai824@gmail.com>
2016-04-20 23:13:08 +0200

    Update TCPserverProgram.cs

--- TCPserverProgram.cs
+++ TCPserverProgram.cs
@@ -16,18 +16,18 @@ namespace TCPserver
             string receiveString = null;
             IPAddress localIP = IPAddress.Parse("127.0.0.1");
             int localPort = 10159;
-            TcpListener listener = new TcpListener(localIP, localPort);//用本地IP和端口实例化Listener
-            Console.WriteLine("我是TCP服务器, 我的名字是{0}", Dns.GetHostName());
-            listener.Start();//开始监听
+            TcpListener listener = new TcpListener(localIP, localPort);//Using local IP and port to initiate Listener 
+            Console.WriteLine("I am TCP server,my name is{0}", Dns.GetHostName());
+            listener.Start();//start to listen
             while (true)
             {
-                client = listener.AcceptTcpClient();//接受一个Client
+                client = listener.AcceptTcpClient();//accept a Client
                 buffer = new byte[client.ReceiveBufferSize];
-                stream = client.GetStream();//获取网络流
-                stream.Read(buffer, 0, buffer.Length);//读取网络流中的数据
-                stream.Close();//关闭流
-                client.Close();//关闭Client
-                receiveString = Encoding.Default.GetString(buffer).Trim('\0');//转换成字符串
+                stream = client.GetStream();//get network stream
+                stream.Read(buffer, 0, buffer.Length);//get the data from network stream
+                stream.Close();//close the network stream
+                client.Close();//close Client
+                receiveString = Encoding.Default.GetString(buffer).Trim('\0');//convert to string
                 Console.WriteLine(receiveString);
             }
 
