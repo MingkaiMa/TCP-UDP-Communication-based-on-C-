d4cafe9375bb90c734e0c1b97a4a489afcd6bf07
Mingkai Ma <mamingkai824@gmail.com>
2016-04-20 23:08:59 +0200

    Update TCPclientProgram.cs

--- TCPclientProgram.cs
+++ TCPclientProgram.cs
@@ -10,33 +10,33 @@ namespace TCPclient
     {
         static void Main(string[] args)
         {
-            string sendString = null;//要发送的字符串
-            byte[] sendData = null;//要发送的字节数组
-            TcpClient client = null;//TcpClient实例
-            NetworkStream stream = null;//网络流
+            string sendString = null;//String that is going to be sent
+            byte[] sendData = null;//Data that is going to be sent
+            TcpClient client = null;//TcpClient instance
+            NetworkStream stream = null;//network stream
 
-            IPAddress remoteIP = IPAddress.Parse("127.0.0.1");//远程主机IP
-            int remotePort = 10159;//远程主机端口
-            Console.WriteLine("我是TCP客户机, 我的名字是{0}", Dns.GetHostName());
-            while (true)//死循环
+            IPAddress remoteIP = IPAddress.Parse("127.0.0.1");//remote IP
+            int remotePort = 10159;//remote port
+            Console.WriteLine("I am TCP client, my name is{0}", Dns.GetHostName());
+            while (true)//Endless loop
             {
-                sendString = Console.ReadLine();//获取要发送的字符串
-                sendData = Encoding.Default.GetBytes(sendString);//获取要发送的字节数组
-                client = new TcpClient();//实例化TcpClient
+                sendString = Console.ReadLine();//get the string
+                sendData = Encoding.Default.GetBytes(sendString);//get the data
+                client = new TcpClient();//create new instance of TcpClient 
                 try
                 {
-                    client.Connect(remoteIP, remotePort);//连接远程主机
+                    client.Connect(remoteIP, remotePort);//connect remote 
                 }
                 catch
                 {
-                    Console.WriteLine("连接超时，服务器没有响应！");//连接失败
+                    Console.WriteLine("连接超时，服务器没有响应！");//connection fail
                     Console.ReadKey();
                     return;
                 }
-                stream = client.GetStream();//获取网络流
-                stream.Write(sendData, 0, sendData.Length);//将数据写入网络流
-                stream.Close();//关闭网络流
-                client.Close();//关闭客户端
+                stream = client.GetStream();//get network stream
+                stream.Write(sendData, 0, sendData.Length);//write the data into network stream
+                stream.Close();//close net network stream
+                client.Close();//close the applicaiton
             }
         }
     }
